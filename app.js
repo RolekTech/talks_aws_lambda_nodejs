3a09dc84be6830a5457a877539628f56c2d24b76
Chuck Rolek <chuck@rolek.tech>
2018-12-08 08:18:22 -0600

    adding html and POST respones

--- app.js
+++ app.js
@@ -10,3 +10,21 @@ module.exports = api;
 api.get('/hello', function () {
     return 'hello world';
 });
+
+
+api.get('/sherpa.html', function () {
+    'use strict';
+    var HTML = '<html><body><h1>Aww Snap.... Some HTML!</h1></body></html>';
+
+    return Promise.resolve().then(function () {
+        return new api.ApiResponse(HTML, {'X-Version': '303', 'Content-Type': 'text/html'}, 400);
+    });
+});
+
+
+api.post('/squiggle', function (request) {
+    'use strict';
+    console.log(request);
+
+    return "he's very FIAT " + JSON.stringify(request);
+}, { success: 201 });
